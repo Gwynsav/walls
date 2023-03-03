d1ac474474f611df3059f6b07db44678a6fcfc95
To grivel <grivel.theo@protonmail.com>
2023-03-03 14:24:22 +0100

    script: list wallpaper in each folder

--- create_readme.sh
+++ create_readme.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+list_wall_of_a_folder () {
+	find "${1}" -type f -print0 | while IFS= read -r -d '' file
+	do
+		printf "\n\n=== %s" "${file}" >> README.adoc
+	done
+}
+
 add_titles_and_preview () {
 	find . -maxdepth 1 -type d \! -name ".git" \! -name "." \! -name "docs" -print0 | while IFS= read -r -d '' file
 	do
 		dir_basename=$(basename "${file}")
 		printf "\n\n== %s" "${dir_basename}" >> README.adoc
+		list_wall_of_a_folder "${dir_basename}"
 	done
 }
 
