c65f1f18df3fd108456f7de60dcc06311dca9e00
To grivel <grivel.theo@protonmail.com>
2023-03-03 14:20:38 +0100

    script: split in function

--- create_readme.sh
+++ create_readme.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Header of the README
-printf "= walls\nPersonal collection of wallpapers." > README.adoc
+add_titles_and_preview () {
+	find . -maxdepth 1 -type d \! -name ".git" \! -name "." \! -name "docs" -print0 | while IFS= read -r -d '' file
+	do
+		dir_basename=$(basename "${file}")
+		printf "\n\n== %s" "${dir_basename}" >> README.adoc
+	done
+}
 
-find . -maxdepth 1 -type d \! -name ".git" \! -name "." \! -name "docs" -print0 | while IFS= read -r -d '' file
-do
-	dir_basename=$(basename "${file}")
-	printf "\n\n== %s" "${dir_basename}" >> README.adoc
-done
+main () {
 
-# Remove the "no newline at the end of file"
-printf "\n" >> README.adoc
+	# Header of the README
+	printf "= walls\nPersonal collection of wallpapers." > README.adoc
+
+	add_titles_and_preview
+
+	# Remove the "no newline at the end of file"
+	printf "\n" >> README.adoc
+}
+
+main
