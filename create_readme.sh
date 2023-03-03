541a66abf447e4dc4474bd4195c43a5640668606
To grivel <grivel.theo@protonmail.com>
2023-03-03 13:47:11 +0100

    script: add titles for every folder

--- create_readme.sh
+++ create_readme.sh
@@ -3,5 +3,11 @@
 # Header of the README
 printf "= walls\nPersonal collection of wallpapers." > README.adoc
 
+for dir in $(find . -maxdepth 1 -type d \! -name ".git" \! -name "." \! -name "docs")
+do
+	dir_basename=$(basename "${dir}")
+	printf "\n\n== %s" "${dir_basename}" >> README.adoc
+done
+
 # Remove the "no newline at the end of file"
 printf "\n" >> README.adoc
